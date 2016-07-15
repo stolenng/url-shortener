88432d258828377d66b6a1a553a2b279a661de10
Georgy Glezer <stolenng@gmail.com>
2016-07-15 10:33:02 +0000

    port changed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 routes(app);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
